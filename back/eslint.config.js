377bfa27982247ef3f1a869a209dbd5d609bee70
Thomas ABADIE <t.abadie@cargo-services.fr>
2026-05-26 09:22:17 +0200

    [INFRA] Configuration ESLint et Prettier + formatage du back

---
+++ back/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'warn',
+      'no-console': 'off',
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+]
