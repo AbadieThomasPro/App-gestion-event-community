da80b7292e855c1a95353c382e3499b6886c55b6
Thomas ABADIE <t.abadie@cargo-services.fr>
2026-06-24 15:18:40 +0200

    Mise en place de la page de registration + gestion JWT + test frontend pour l'inscription et la connexion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import HomePage from './pages/home/HomePage'
 import LoginPage from './pages/login/LoginPage'
+import RegisterPage from './pages/register/RegisterPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
