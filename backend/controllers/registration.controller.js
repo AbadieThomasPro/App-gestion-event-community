c8045f8dad91c0ce429dd6f70df486903b96d6bb
Thomas ABADIE <t.abadie@cargo-services.fr>
2026-06-30 09:35:33 +0200

    Route POST /events/:id/register + DELETE /events/:id/register + GET /events/:id/registrations

---
+++ backend/controllers/registration.controller.js
@@ -0,0 +1,41 @@
+import {
+  listRegistrations,
+  registerForEvent,
+  unregisterFromEvent,
+} from '../services/registration.service.js'
+import { HttpError } from '../utils/http-error.js'
+
+function handleError(error, res) {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({ message: error.message })
+  }
+
+  throw error
+}
+
+export async function postRegistration(req, res) {
+  try {
+    const registration = await registerForEvent(req.params.id, req.user.id)
+    res.status(201).json(registration)
+  } catch (error) {
+    handleError(error, res)
+  }
+}
+
+export async function removeRegistration(req, res) {
+  try {
+    await unregisterFromEvent(req.params.id, req.user.id)
+    res.status(204).send()
+  } catch (error) {
+    handleError(error, res)
+  }
+}
+
+export async function getRegistrations(req, res) {
+  try {
+    const registrations = await listRegistrations(req.params.id)
+    res.json(registrations)
+  } catch (error) {
+    handleError(error, res)
+  }
+}
